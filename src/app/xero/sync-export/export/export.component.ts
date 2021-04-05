c05298dccd1382808d6eee687b859b4a2d7fb632
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-04-05 11:30:30 +0530

    Polling changes for new task state (#26)

--- src/app/xero/sync-export/export/export.component.ts
+++ src/app/xero/sync-export/export/export.component.ts
@@ -76,9 +76,9 @@ export class ExportComponent implements OnInit {
     const that = this;
     interval(3000).pipe(
       switchMap(() => from(that.taskService.getAllTasks('ALL'))),
-      takeWhile((response) => response.results.filter(task => task.status === 'IN_PROGRESS'  && task.type !== 'FETCHING_EXPENSES' &&  filteredIds.includes(task.expense_group)).length > 0, true)
+      takeWhile((response) => response.results.filter(task => (task.status === 'IN_PROGRESS' || task.status === 'ENQUEUED')  && task.type !== 'FETCHING_EXPENSES' &&  filteredIds.includes(task.expense_group)).length > 0, true)
     ).subscribe((res) => {
-      if (res.results.filter(task => task.status === 'IN_PROGRESS'  && task.type !== 'FETCHING_EXPENSES' &&  filteredIds.includes(task.expense_group)).length === 0) {
+      if (res.results.filter(task => (task.status === 'IN_PROGRESS' || task.status === 'ENQUEUED')  && task.type !== 'FETCHING_EXPENSES' &&  filteredIds.includes(task.expense_group)).length === 0) {
         that.taskService.getAllTasks('FAILED').subscribe((taskResponse) => {
           that.failedExpenseGroupCount = taskResponse.count;
           that.successfulExpenseGroupCount = filteredIds.length - that.failedExpenseGroupCount;
