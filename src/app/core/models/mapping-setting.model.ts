c5c7fff7dfa2f3c846a12da49b8098355df36845
labhvam5 <88420539+labhvam5@users.noreply.github.com>
2021-11-22 15:27:02 +0530

    select custom field (#48)

--- src/app/core/models/mapping-setting.model.ts
+++ src/app/core/models/mapping-setting.model.ts
@@ -1,11 +1,12 @@
 /* tslint:disable */
 // TODO: Use something for serialization / deserialization
 export type MappingSetting = {
-    id?: number;
-    source_field: string;
-    destination_field: string;
-    created_at?: Date;
-    updated_at?: Date;
-    workspace?: number;
-  };
-  
+  id?: number;
+  source_field: string;
+  destination_field: string;
+  is_custom?: boolean;
+  import_to_fyle?: boolean;
+  created_at?: Date;
+  updated_at?: Date;
+  workspace?: number;
+};
--- src/app/xero/settings/xero-configurations/expense-field-configuration/expense-field-configuration.component.ts
+++ src/app/xero/settings/xero-configurations/expense-field-configuration/expense-field-configuration.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { SettingsService } from 'src/app/core/services/settings.service';
-import { forkJoin } from 'rxjs';
 import { MappingsService } from 'src/app/core/services/mappings.service';
 import { RxwebValidators } from '@rxweb/reactive-form-validators';
 import { WindowReferenceService } from 'src/app/core/services/window.service';
 import { XeroComponent } from 'src/app/xero/xero.component';
 import { MappingSetting } from 'src/app/core/models/mapping-setting.model';
 import { ExpenseField } from 'src/app/core/models/expense-field.model';
+import { MappingSettingResponse } from 'src/app/core/models/mapping-setting-response.model';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-expense-field-configuration',
@@ -17,38 +18,51 @@ import { ExpenseField } from 'src/app/core/models/expense-field.model';
 })
 export class ExpenseFieldConfigurationComponent implements OnInit {
   expenseFieldsForm: FormGroup;
-  expenseFields: FormArray;
   workspaceId: number;
   isLoading: boolean;
   mappingSettings: MappingSetting[];
   fyleExpenseFields: ExpenseField[];
   xeroFields: ExpenseField[];
-  fyleFormFieldList: ExpenseField[];
-  xeroFormFieldList: ExpenseField[];
   windowReference: Window;
+  customFieldForm: FormGroup;
+  showCustomFieldName: boolean;
+  customFieldName  = 'Choose Fyle Expense field';
+  isSystemField: boolean;
+  showAddButton: boolean;
+  xeroFormFieldList: ExpenseField[];
 
-  constructor(private formBuilder: FormBuilder, private route: ActivatedRoute, private router: Router, private settingsService: SettingsService, private mappingsService: MappingsService, private xero: XeroComponent, private windowReferenceService: WindowReferenceService) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private settingsService: SettingsService,
+    private mappingsService: MappingsService,
+    private xero: XeroComponent,
+    private windowReferenceService: WindowReferenceService,
+    private snackBar: MatSnackBar) {
     this.windowReference = this.windowReferenceService.nativeWindow;
    }
 
-  createExpenseField(sourceField: string = '', destinationField: string = '') {
+  createExpenseField(sourceField: string = '', destinationField: string = '', isCustom: boolean = false, importToFyle: boolean = false) {
     const that = this;
 
     const group = that.formBuilder.group({
       source_field: [sourceField ? sourceField : '', [Validators.required, RxwebValidators.unique()]],
       destination_field: [destinationField ? destinationField : '', [Validators.required, RxwebValidators.unique()]],
+      import_to_fyle: [importToFyle],
+      is_custom: [isCustom],
     });
 
     if (sourceField && destinationField) {
-      group.disable();
+      group.controls.source_field.disable();
+      group.controls.destination_field.disable();
     }
     return group;
   }
 
-  showAddButton() {
+  showOrHideAddButton() {
     const that = this;
-    const expenseFieldCount = that.expenseFieldsForm.controls.expenseFields.value.length;
-    if (expenseFieldCount === Math.min(that.fyleExpenseFields.length, that.xeroFields.length) || (expenseFieldCount > 2)) {
+    if (that.expenseFieldsForm.controls.expenseFields.value.length === that.xeroFields.length || that.showCustomFieldName) {
       return false;
     }
     return true;
@@ -57,65 +71,200 @@ export class ExpenseFieldConfigurationComponent implements OnInit {
   addExpenseField() {
     const that = this;
 
-    that.expenseFields = that.expenseFieldsForm.get('expenseFields') as FormArray;
-    that.expenseFields.push(that.createExpenseField());
+    const expenseFieldsFormsArray = that.expenseFieldsForm.get('expenseFields') as FormArray;
+    expenseFieldsFormsArray.push(that.createExpenseField());
+    that.showAddButton = that.showOrHideAddButton();
   }
 
   saveExpenseFields() {
     const that = this;
 
-    that.isLoading = true;
-    const expenseFields: MappingSetting[] = that.expenseFieldsForm.value.expenseFields;
+    if (that.expenseFieldsForm.valid) {
+      that.isLoading = true;
+      // getRawValue() would have values even if they are disabled
+      const expenseFields = that.expenseFieldsForm.getRawValue().expenseFields;
 
-    that.settingsService.postMappingSettings(that.workspaceId, expenseFields).subscribe(response => {
-      that.xero.getGeneralSettings();
-      that.router.navigateByUrl(`/workspaces/${that.workspaceId}/dashboard`);
-      that.isLoading = false;
+      let hasCustomField = false;
+      expenseFields.forEach(element => {
+        if (element.source_field !== 'PROJECT' && element.source_field !== 'COST_CENTER' && !element.is_custom) {
+          element.is_custom = true;
+        }
+        if (element.is_custom) {
+          hasCustomField = true;
+        }
+      });
+
+      that.settingsService.postMappingSettings(that.workspaceId, expenseFields).subscribe((mappingSetting: MappingSetting[]) => {
+        that.xero.refreshDashboardMappingSettings(mappingSetting);
+        that.createFormFields(mappingSetting);
+        if (hasCustomField) {
+          that.getFyleFields().then(() => {
+            that.isLoading = false;
+          });
+        } else {
+          that.isLoading = false;
+        }
+      }, (error) => {
+        if (error.error && 'message' in error.error && error.error.message === 'Duplicate custom field name') {
+          const fieldName = error.error.field_name.replace(/_/g, ' ').toLowerCase().replace(/\b(\w)/g, s => s.toUpperCase());
+          that.snackBar.open(`${fieldName} already exists in Fyle, try creating a custom field with different name`,
+          '', {
+            duration: 5000
+          });
+        } else {
+          that.snackBar.open('Something went wrong while saving expense fields mapping');
+        }
+        that.getSettings();
+      });
+    } else {
+      that.snackBar.open('Please fill all mandatory fields');
+    }
+  }
+
+  removeExpenseField(index: number, sourceField: string = null) {
+    const that = this;
+
+    that.showCustomFieldName = false;
+    const expenseFields = that.expenseFieldsForm.get('expenseFields') as FormArray;
+    expenseFields.removeAt(index);
+
+    // remove custom field option from the Fyle fields drop down if the corresponding row is deleted
+    if (sourceField && sourceField !== 'PROJECT' && sourceField !== 'COST_CENTER') {
+      that.fyleExpenseFields = that.fyleExpenseFields.filter(mappingRow => mappingRow.attribute_type !== sourceField);
+    }
+    that.showAddButton = that.showOrHideAddButton();
+  }
+
+  showCustomField(expenseField) {
+    const that = this;
+    expenseField.controls.import_to_fyle.setValue(true);
+    expenseField.controls.import_to_fyle.disable();
+    expenseField.controls.source_field.disable();
+
+    that.showCustomFieldName = true;
+    that.customFieldForm.markAllAsTouched();
+  }
+
+  updateCustomFieldName(name: string) {
+    const that = this;
+
+    let existingFields: string[] = that.fyleExpenseFields.map(fields => fields.display_name.toLowerCase());
+    const systemFields = ['employee id', 'organisation name', 'employee name', 'employee email', 'expense date', 'expense date', 'expense id', 'report id', 'employee id', 'department', 'state', 'reporter', 'report', 'purpose', 'vendor', 'category', 'category code', 'mileage distance', 'mileage unit', 'flight from city', 'flight to city', 'flight from date', 'flight to date', 'flight from class', 'flight to class', 'hotel checkin', 'hotel checkout', 'hotel location', 'hotel breakfast', 'currency', 'amount', 'foreign currency', 'foreign amount', 'tax', 'approver', 'project', 'billable', 'cost center', 'cost center code', 'approved on', 'reimbursable', 'receipts', 'paid date', 'expense created date'];
+    existingFields = existingFields.concat(systemFields);
+
+    if (existingFields.indexOf(name.toLowerCase()) !== -1) {
+      that.isSystemField = true;
+      return;
+    }
+
+    that.isSystemField = false;
+    that.customFieldName = name;
+  }
+
+  hideCustomField(event: string) {
+    const that = this;
+
+    that.showCustomFieldName = false;
+    const lastAddedMappingIndex = that.expenseFieldsForm.getRawValue().expenseFields.length - 1;
+    const customFieldName = that.customFieldForm.value.customFieldName.replace(/ /g, '_').toUpperCase();
+
+    if (event === 'Done') {
+      that.fyleExpenseFields.push({
+        attribute_type: customFieldName,
+        display_name: that.customFieldForm.value.customFieldName
+      });
+
+      const formValuesArray = that.expenseFieldsForm.get('expenseFields') as FormArray;
+      formValuesArray.controls[lastAddedMappingIndex].get('source_field').setValue(customFieldName);
+      formValuesArray.controls[lastAddedMappingIndex].get('is_custom').setValue(true);
+      formValuesArray.controls[lastAddedMappingIndex].get('import_to_fyle').setValue(true);
+
+    } else if (lastAddedMappingIndex) {
+      that.removeExpenseField(lastAddedMappingIndex);
+    }
+
+    that.customFieldForm.controls.customFieldName.reset();
+    that.customFieldName = 'Choose Fyle Expense field';
+    that.showAddButton = that.showOrHideAddButton();
+  }
+
+  saveCustomField() {
+    const that = this;
+
+    that.showCustomFieldName = false;
+    that.saveExpenseFields();
+  }
+
+  createFormFields(mappingSetting: MappingSetting[]) {
+    const that = this;
+    that.mappingSettings = mappingSetting.filter(
+      setting => setting.source_field !== 'EMPLOYEE' && setting.source_field !== 'CATEGORY' && setting.source_field !== 'CORPORATE_CARD'
+    );
+
+    let expenseFieldFormArray;
+    if (that.mappingSettings.length) {
+      expenseFieldFormArray = that.mappingSettings.map(
+        setting => that.createExpenseField(setting.source_field, setting.destination_field, setting.is_custom, setting.import_to_fyle)
+      );
+    } else {
+      expenseFieldFormArray = [that.createExpenseField()];
+    }
+
+    that.expenseFieldsForm = that.formBuilder.group({
+      expenseFields: that.formBuilder.array(expenseFieldFormArray)
+    });
+
+  }
+
+  getMappingSettings() {
+    const that = this;
+
+    return that.settingsService.getMappingSettings(that.workspaceId).toPromise().then((mappingSetting: MappingSettingResponse) => {
+      that.createFormFields(mappingSetting.results);
+
+      return mappingSetting;
     });
   }
 
-  removeExpenseField(index: number) {
+  getFyleFields() {
     const that = this;
 
-    const expenseFields = that.expenseFieldsForm.get('expenseFields') as FormArray;
-    expenseFields.removeAt(index);
+    return that.mappingsService.getFyleExpenseFields().toPromise().then((fyleFields: ExpenseField[]) => {
+      that.fyleExpenseFields = fyleFields;
+
+      return fyleFields;
+    });
+  }
+
+  getXeroFields() {
+    const that = this;
+
+    return that.mappingsService.getXeroFields().toPromise().then((xeroFields: ExpenseField[]) => {
+      console.log(xeroFields)
+      that.xeroFields = xeroFields;
+      that.xeroFormFieldList = xeroFields;
+
+      return xeroFields;
+    });
   }
 
   getSettings() {
     const that = this;
-    forkJoin(
-      [
-        that.settingsService.getMappingSettings(that.workspaceId),
-        that.mappingsService.getFyleExpenseFields(),
-        that.mappingsService.getXeroFields()
-      ]
-    ).subscribe(response => {
-      that.mappingSettings = response[0].results.filter(
-        setting => setting.source_field !== 'EMPLOYEE' && setting.source_field !== 'CATEGORY' && setting.source_field !== 'CORPORATE_CARD'
-      );
 
-      let expenseFieldFormArray;
-
-      that.fyleExpenseFields = response[1];
-      that.xeroFields = response[2];
-
-      that.fyleFormFieldList = response[1];
-      that.xeroFormFieldList = response[2];
-
-      if (that.mappingSettings.length) {
-        expenseFieldFormArray = that.mappingSettings.map(
-          setting => that.createExpenseField(setting.source_field, setting.destination_field)
-        );
-      } else {
-        expenseFieldFormArray = [that.createExpenseField()];
-      }
-
-      that.expenseFieldsForm = that.formBuilder.group({
-        expenseFields: that.formBuilder.array(expenseFieldFormArray)
-      });
-
-      that.isLoading = false;
+    that.customFieldForm = that.formBuilder.group({
+      customFieldName : ['', Validators.required]
     });
+
+    that.getMappingSettings()
+      .then(() => {
+        return that.getFyleFields();
+      })
+      .then(() => {
+        return that.getXeroFields();
+      }).finally(() => {
+        that.showAddButton = that.showOrHideAddButton();
+        that.isLoading = false;
+      });
   }
 
   ngOnInit() {
